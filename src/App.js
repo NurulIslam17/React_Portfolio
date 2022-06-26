a4007515ec8f1f312cb8193fd041147ab37dc21f
Md Nurul Islam <98460077+NurulIslam17@users.noreply.github.com>
2022-06-26 11:14:33 +0600

    Adding Navbar , logo and routung

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Home from "./Component/Home";
 import About from "./Component/About";
 import Skill from "./Component/Skills";
@@ -7,17 +9,18 @@ import Service from "./Component/Service";
 import Contact from "./Component/Contact";
 import Menubar from "./Component/Menubar";
 import NoPage from "./Component/NoPage";
+import './Component/Style.css';
 
 function App() {
   return (
     <>
       <Menubar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skill" element={<Skill />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/skill" element={<Skill />} />
+        <Route exact path="/service" element={<Service />} />
+        <Route exact path="/contact" element={<Contact />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </>
--- src/Component/Menubar.jsx
+++ src/Component/Menubar.jsx
@@ -1,16 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Menubar() {
   return (
     <>
-      <ul>
-        <Link to="/"> Home </Link>
-        <Link to="about"> About </Link>
-        <Link to="skill"> Skill </Link>
-        <Link to="service"> Service </Link>
-        <Link to="contact"> Contact </Link>
-      </ul>
+      <div className="container-fluid nav-bg">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <nav className="navbar navbar-expand-lg bg-white">
+              <div className="container-fluid">
+                <NavLink className="navbar-brand" to="/">
+                  <span className="Logo"><span className="tech">Tech</span><span className="nur">Nur</span></span>
+                </NavLink>
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div
+                  className="collapse navbar-collapse"
+                  id="navbarSupportedContent"
+                >
+                  <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/">
+                        Home
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/about">
+                        About
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/skill">
+                        Skill
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/service">
+                        Service
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/contact">
+                        Contact
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
